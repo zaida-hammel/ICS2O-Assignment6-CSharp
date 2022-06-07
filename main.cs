3a89dda428ce55e9db029ca1bb1a122a2e8298f1
Zaida Hammel <99354763+zaida-hammel@users.noreply.github.com>
2022-06-07 21:14:25 +0000

    edited code

--- main.cs
+++ main.cs
@@ -1,8 +1,3 @@
-// Created by: Zaida Hammel
-// Created on: May 2022
-//
-// This program accepts user input
-
 using System;
 using System.Reflection;
 using System.Collections.Generic;
@@ -13,17 +8,17 @@ using System.Text.Json.Nodes;
 
 class Program
 {
-    public static async Task Main(string[] args)
+    public static async Task Main()
     {
         HttpClient client = new HttpClient();
         string response = await client.GetStringAsync(
             "https://shibe.online/api/shibes?count=[1-100]"
         );
-        var json = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
-        JsonNode jsonData = JsonNode.Parse(response)!;
-        JsonNode url = jsonData[0]["url"];
+        var shibaUrl = System.Text.Json.JsonSerializer.Deserialize<Object>(response);
+        Console.WriteLine("");
+        JsonNode resultNode = JsonNode.Parse(response)!;
 
-        Console.WriteLine("Shiba Inu picture " + url + " .");
-        Console.WriteLine("\nDone");
+        Console.WriteLine("Shiba URL is: " + shibaUrl + ".");
+        Console.WriteLine("\nDone.");
     }
 }
